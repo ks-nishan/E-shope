edb2372c940446259df0bd1438010fcd43ad40bb
nishanthan <nishanthan@codevus.com>
2023-06-24 12:07:58 +0530

    signUp page done

--- frontend/src/components/Signup/Signup.jsx
+++ frontend/src/components/Signup/Signup.jsx
@@ -2,22 +2,54 @@ import { React, useState } from "react";
 import { AiOutlineEye, AiOutlineEyeInvisible } from "react-icons/ai";
 import styles from "../../styles/styles";
 import { Link } from "react-router-dom";
+import { RxAvatar } from "react-icons/rx";
 
-const Login = () => {
+const Signup = () => {
   const [email, setEmail] = useState("");
+  const [name, setName] = useState("");
   const [password, setPassword] = useState("");
   const [visible, setVisibile] = useState(false);
+  const [avatar, setAvatar] = useState(null);
+
+  const handelSubmit = () => {
+    console.log("ggfgh");
+  };
+
+  const handleFileInputChange = (e) => {
+    const file = e.target.files[0];
+    setAvatar(file);
+  };
 
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
       <div className="sm:mx-auto sm:w-full sm:max-w-md">
         <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
-          Login to your account
+          Register as a New User!
         </h2>
       </div>
       <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
         <div className="bg-white py-8 px-4 shadow sm:rounded-lg sm:ox-10">
           <form action="" className="space-y-6">
+            <div>
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium text-gray-700"
+              >
+                Full Name
+              </label>
+              <div className="mt-1">
+                <input
+                  type="text"
+                  name="text"
+                  autoCapitalize="name"
+                  required
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                  className="appearance-none block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm placegolder-gray-400 focus:outline-none focus:ring-blue-500 focus:border-blue-500:text:sm"
+                />
+              </div>
+            </div>
+
             <div>
               <label
                 htmlFor="email"
@@ -71,43 +103,54 @@ const Login = () => {
                 )}
               </div>
             </div>
-            <div className={`${styles.noramlFlex} justify-between`}>
-              <div className={`${styles.noramlFlex}`}>
-                <input
-                  type="checkbox"
-                  name="remember-me"
-                  id="remember-me"
-                  className="h-4 w-4 text-blue-700 focus:ring-blue-500 border-gray-300 rounded"
-                />
+
+            <div className="" justify-around>
+              <label
+                htmlFor="avatar"
+                className="block text-sm font-medium text-gray-700"
+              ></label>
+              <div className=" mt-2 flex item-center">
+                <span className="inline-block h-8 w-8 roundaed-full overflow-hidden">
+                  {avatar ? (
+                    <img
+                      src={URL.createObjectURL(avatar)}
+                      alt="avatar"
+                      className="h-full w-full object-cover rounded-full"
+                    />
+                  ) : (
+                    <RxAvatar className="h-8 w-8" />
+                  )}
+                </span>
                 <label
-                  htmlFor="remember-me"
-                  className="ml-2 block text-sm text-gray-900"
+                  htmlFor="file-input"
+                  className="ml-5 flex item-center justify-center px-4 py-2 border border-gray-300 rounded-md shadow-md text-sm font-mediumtext-gray-700 bg-white hover:bg-gray-50"
                 >
-                  Remember Me
+                  <span>Upload File</span>
+                  <input
+                    type="file"
+                    name="avatar"
+                    id="file-input"
+                    accept=".jpg,.jpeg,.png"
+                    onChange={handleFileInputChange}
+                    className="sr-only"
+                  />
                 </label>
               </div>
-              <div className="text-sm">
-                <a
-                  href=".forgot-password"
-                  className="text-blue-600 hover:text-blue-500 font-medium"
-                >
-                  Forgot Your Password?
-                </a>
-              </div>
             </div>
+
             <div className="">
               <button
                 type="submit"
                 className="group relative w-full h-[40px] flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-blue-600 hover:bg-blue-700"
               >
-                Login
+                Sign Up
               </button>
             </div>
             <div className={`${styles.noramlFlex}  `}>
               <h4>
-                Not have an account?{" "}
-                <Link to="/sign-up" className="text-blue-600 pl-2">
-                  Sign Up
+                Have an account?{" "}
+                <Link to="/login" className="text-blue-600 pl-2">
+                  Log In
                 </Link>
               </h4>
             </div>
@@ -118,4 +161,4 @@ const Login = () => {
   );
 };
 
-export default Login;
+export default Signup;
